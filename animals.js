21902dad7020359045855f74f7ad214cf6277d10
Herzele <dignoire.cyprien@hotmail.fr>
2022-07-01 12:17:38 +0200

    Add files via upload

---
+++ animals.js
@@ -0,0 +1,17 @@
+
+class Animals{
+	constructor (params){
+
+	this.aniName = params.aniName;
+	this.aniType = params.aniType;
+	this.aniSize = params.aniSize;
+
+
+	animalList.push(this);
+
+	}
+}
+
+var loadedList = [];
+var animalList = [];
+
---
+++ enclosures.js
@@ -0,0 +1,15 @@
+
+class Enclosure{
+	constructor (params){
+
+	this.encSize = params.encSize;
+
+
+	enclosureList.push(this);
+
+	}
+}
+
+var loadedList = [];
+var enclosureList = [];
+
